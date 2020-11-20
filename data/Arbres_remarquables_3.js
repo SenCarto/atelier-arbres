b4b58e94226d32369edcf88d987f0460ee339292
SenCarto <72811508+SenCarto@users.noreply.github.com>
2020-11-20 20:51:12 +0100

    Add files via upload

---
+++ data/Arbres_remarquables_3.js
@@ -0,0 +1,9 @@
+var json_Arbres_remarquables_3 = {
+"type": "FeatureCollection",
+"name": "Arbres_remarquables_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nom_arbre": "Collection de pommiers", "Espece": "Pommier", "Statut": "Verger", "Localisati": "Rue de 44ème Régiment des travailleurs sénégalais", "Photos": "Crouy-saint-pierre.jpg" }, "geometry": { "type": "Point", "coordinates": [ 2.082833831873879, 49.972768292331963 ] } },
+{ "type": "Feature", "properties": { "Nom_arbre": "Maronnier", "Espece": "Maronnier", "Statut": "Arbre seul", "Localisati": "route d’Halloy-les-Pernois", "Photos": "Berteaucourt.jpg" }, "geometry": { "type": "Point", "coordinates": [ 2.155683327311415, 50.039954279198049 ] } }
+]
+}
